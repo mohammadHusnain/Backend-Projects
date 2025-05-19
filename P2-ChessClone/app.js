9131790c7f006926967a0e6793f233ee2e43cfc1
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-05-19 16:55:27 +0500

    updated files

--- P2-ChessClone/app.js
+++ P2-ChessClone/app.js
@@ -23,6 +23,48 @@ app.get('/', (req, res) => {
 io.on('connection', function(uniqueSocket){
 console.log('new connection setted up');
 
+// setting up the game state for players
+
+if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit('playerRole', 'white'); 
+}
+
+else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit('playerRole', 'black');
+} else {
+    uniqueSocket.emit('spectatorRole');
+    return;
+}
+
+// game state if a player leaves the game
+
+uniqueSocket.on('disconnect', function(){
+    if (uniqueSocket.id === players.white) {
+        delete players.white;
+    } 
+    else if (uniqueSocket.id === players.black) {
+        delete players.black;
+    }
+});
+
+// game state if a player makes a move and if move is valid or not
+
+try {
+    
+
+
+// data received from frontend
+
+// uniqueSocket.on("creatine", function(){  
+// io.emit("creatine and protein")
+// });
+
+// uniqueSocket.on('disconnect' , function(){
+//     console.log('user disconnected');   
+// });
+
 });
 
 server.listen(3000, () => {
--- P2-ChessClone/public/js/chessGame.js
+++ P2-ChessClone/public/js/chessGame.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+// socket.emit("creatine"); //creatine named event is based and built upon frontend and will be sent from here to the backend
+// socket.on('creatine and protein', function(){
+//     console.log('creatine and protein');
+// }); 
+
